6f5f56c122008625df1b4900010f27e5f8b955a1
john <johnzorrillx@gmail>
2023-11-18 21:57:04 -0500

    a

--- logica.js
+++ logica.js
@@ -7,27 +7,3 @@ document.getElementById("login-button").addEventListener("click", function() {
     document.querySelector(".panel-interfaz").style.display = "block";
 });
 
-/* Focus y Blur para los Inputs */
-document.getElementById("username").addActionListener("focus", function() {
-    if(this.value == "username"){
-        this.value = "";
-    }
-});
-
-document.getElementById("username").addActionListener("blur", function(){
-    if(this.value == ""){
-        this.value = "username";
-    }
-});
-
-document.getElementById("password").addActionListener("focus", function(){
-    if (this.value == "password") {
-        this.value = "";
-    }
-});
-
-document.getElementById("password").addActionListener("blur", function(){
-    if(this.value == ""){
-        this.value = "password";
-    }
-});
